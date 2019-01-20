12453a1e67f0b9fa53e801069964e3005f69a474
cassaram09 <cassaram09@gmail.com>
2019-01-20 18:49:19 -0500

    update app to render Questions component; import questions scss

--- src/app/app.js
+++ src/app/app.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 
 import './app.scss';
 
+import DATA from 'utils/mockData'
+
+import { Questions } from 'questions'
+
 class App extends Component {
   render() {
     return (
       <div className="app">
         <div className="container">
-         
+        
+          <Questions questionList={DATA} />
         </div>
       </div>
     );
