fe4b208c2ca8f1959ddb8290f898e281cb0931cd
cassaram09 <cassaram09@gmail.com>
2019-01-20 16:42:24 -0500

    install and test bulma; clean up stock code from CRA

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 
-import logo from 'assets/images/logo.svg';
 import './app.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> 
+      <div className="app">
+        <div className="container">
+         
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Styles
+import 'bulma/css/bulma.css'
 import 'index.scss';
+
 import { App } from './app';
 import * as serviceWorker from './serviceWorker';
 
