c7b46a68a20f367122469e3dc7f6f87b3f5cf649
cassaram09 <cassaram09@gmail.com>
2019-01-20 18:47:35 -0500

    fix typos in mock data

--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -1,23 +1,24 @@
 const DATA = [
   {
-    id: 1
+    id: 1,
     question: 'How much wood could a woodchuck chuck if a woodchuck could chuck wood?',
-    type: "multipleChoice",
-    answer: [
+    multiChoice: [
       "12 logs",
       "1 branch",
       "Not as much as a beaver",
       "Woodchucks can't chuck wood, silly!"
-    ]
+    ],
+    type: "multiChoice",
+    answer: 0
   },
   {
-    id: 2
+    id: 2,
     question: 'How much does an elephant weigh?',
     type: "shortText",
     answer: "Enough to break the ice."
   },
   {
-    id: 2
+    id: 3,
     question: 'Explain critical theory.',
     type: "longText",
     answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
