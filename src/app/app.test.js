1b0405c9bcd57061bc4647e7b69dd704b3e87554
cassaram09 <cassaram09@gmail.com>
2019-01-20 11:01:08 -0500

    configure enzyme and jest; run tests

--- src/app/app.test.js
+++ src/app/app.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow, render, mount } from 'utils/enzyme';
 
 import App from './app';
 
 describe('App', () => {
   it('renders without crashing', () => {
-    const component = shallow(<App />);
+    const app = shallow(<App />);
   
-    expect(component).toMatchSnapshot();
+    expect(app).toMatchSnapshot();
+  });
+
+  it('has the correct text', () => {
+    const app = render(<App />);
+    const p = app.find('p'); 
+    
+    expect(p.text()).toBe('Edit src/App.js and save to reload.'); // fails,
   });
 });
