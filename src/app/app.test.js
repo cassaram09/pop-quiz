6b1b9b03c8ac2bbd59d37820a2add6fc49b0a5e7
cassaram09 <cassaram09@gmail.com>
2019-01-20 08:14:32 -0500

    installing test utils

--- src/app/app.test.js
+++ src/app/app.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './app';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from './app';
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const component = shallow(<App />);
+  
+    expect(component).toMatchSnapshot();
+  });
 });
